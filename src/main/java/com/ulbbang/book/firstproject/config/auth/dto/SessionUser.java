d2a2cf36679764bdef93225f7dc3e2a43ea78a85
oculo0204 <oculo0204@gmail.com>
2025-01-13 15:34:25 +0900

    5단원까지 로그인 테스트 전까지

---
+++ src/main/java/com/ulbbang/book/firstproject/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.ulbbang.book.firstproject.config.auth.dto;
+
+import com.ulbbang.book.firstproject.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
